ebbf520a4bdb430a0586c577c6afd3ed81b76ff0
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2025-03-14 17:04:50 -0400

    create

---
+++ Xcode/BadUI/BadUI/BadUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadUIApp.swift
+//  BadUI
+//
+//  Created by Fayaz Shaikh on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct BadUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
